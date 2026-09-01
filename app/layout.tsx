971a417a808bddbce8362e35e71280c5ff8a49fd
Carlos Aguayo Arrabal <carlosaguayoarrabal@gmail.com>
2026-09-01 20:01:27 +0200

    Fix typo in metadata title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Momentum Digital",
-  description: "Momentum Digital",
+  title: "Momentun Digital",
+  description: "Momentun Digital",
 };
 
 export default function RootLayout({
@@ -17,4 +17,3 @@ export default function RootLayout({
     </html>
   );
 }
-
